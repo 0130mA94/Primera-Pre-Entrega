a2d7c3db8c483657e87126696367fe071a3063b1
marco londero <londero.m.a@gmail.com>
2023-02-08 21:34:28 +0100

    se agrega const y function

--- main.js
+++ main.js
@@ -21,4 +21,42 @@ if((NombreUsuario === "Coder") &&( PasswordIngresado === "House")){
 }
 }
 
+let DetectiveComics = {
+    nombre: "Muerte en la Familia",
+    edición: "Black Label",
+    disponibilidad: true,
+    cantidad: 30,
+    precio: 12,
+    
+}
+console.log(DetectiveComics);
+
+let MarvelComics = {
+    nombre: "House of M",
+    Edición: "Must-Have",
+    disponibilidad: true,
+    cantidad: 25,
+    pecio: 23,
+}
+console.log(MarvelComics);
+
+function Comic(editorial, nombre,edición,precio,cantidad,disponibilidad){
+    this.editorial = editorial;
+    this.nombre = nombre;
+    this.edición = edición;
+    this.precio = precio;
+    this.cantidad = cantidad;
+}
+
+const comic1 = new Comic("Marvel Comics", "Amazing Spiderman: La última cacería de Kraven", "Must-Have", 14, 15);
+const comic2 = new Comic("DC", "Batman: Año Uno", "Black Label", 12, 20);
+const comic3 = new Comic("DC", "Batman: La corte de los buhos", "Deluxe", 20, 11 );
+const comic4 = new Comic("DC", "Batman: Death Metal","Black Label" ,12.50, 7 );
+
+console.log(comic1);
+console.log(comic2);
+console.log(comic3);
+console.log(comic4);
+
+
 
